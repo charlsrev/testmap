9d339cdb2281b398f162e9120e5b5da1286595d4
charlsrev <carlos.san.martin.martinez.de@gmail.com>
2015-08-28 13:19:34 -0300

    androinmasios

--- myApp/www/js/map.js
+++ myApp/www/js/map.js
@@ -1,3 +1,4 @@
+
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
@@ -36,3 +37,5 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
 }
+
+   
